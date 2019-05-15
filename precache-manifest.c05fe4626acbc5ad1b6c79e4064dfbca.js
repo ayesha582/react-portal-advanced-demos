7969e05f98d41a476012fad7b2fc06f5a9058af1
ayesha.m <ayesha.m@media.net>
2019-05-15 19:13:45 +0530

    Updates

---
+++ precache-manifest.c05fe4626acbc5ad1b6c79e4064dfbca.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff39a36d3132da1d2b8d89e415897b35",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2d0acd12d49c08fd9043.bundle.js"
+  },
+  {
+    "revision": "b7753c4ed32093058e78",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.2d0acd12d49c08fd9043.bundle.js"
+  },
+  {
+    "url": "vendors~main.2d0acd12d49c08fd9043.bundle.js"
+  }
+]);
